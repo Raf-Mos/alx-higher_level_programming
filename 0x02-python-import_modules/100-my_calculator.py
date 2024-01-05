ac62503a936d9a694ce32651d7728b32ad82b78c
Rafiki_Mostafa <rafikimostafa@gmail.com>
2024-01-05 05:27:57 +0100

    Advanced tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,15 +4,15 @@ if __name__ == "__main__":
 
     from calculator_1 import add, sub, mul, div
     import sys
-    
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-        
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     operator = sys.argv[2]
-    
+
     if operator == "+":
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif operator == "-":
