6cc338192b2f50303a267303bd0ce31877ad724a
Rafiki_Mostafa <rafikimostafa@gmail.com>
2024-01-05 04:10:07 +0100

    Mandatory tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import sys
 
     som = 0
-    for i in range(1 , len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         som += int(sys.argv[i])
     print("{}".format(som))
