8b64699a3d8fc29e9d8f5ef8d30088aee30a58cc
Rafiki_Mostafa <rafikimostafa@gmail.com>
2024-01-09 03:11:55 +0100

    Mandatory tasks

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for x in range(len(my_list) - 1, -1, -1):
-        print("{}".format(my_list[x]))
+    if my_list:
+        rev_list = my_list[::-1]
+        for x in rev_list:
+            print("{:d}".format(x))
