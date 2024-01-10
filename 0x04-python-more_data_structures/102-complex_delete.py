462d1b7a51afa7d557872922f62cc3e299b241e9
Rafiki_Mostafa <rafikimostafa@gmail.com>
2024-01-11 00:29:56 +0100

    Advanced tasks

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for i, v in a_dictionary:
-        if v == value:
-            del a_dictionary[i]
+    if not a_dictionary:
+        return a_dictionary
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
     return a_dictionary
