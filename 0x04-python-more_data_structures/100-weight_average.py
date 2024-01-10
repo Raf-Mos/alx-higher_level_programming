16bc6bad8ad24fdb356b8d9b12b0273dcf556223
Rafiki_Mostafa <rafikimostafa@gmail.com>
2024-01-11 00:13:35 +0100

    Advanced tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-        weight = (sum(x * y) / sum(y)) for (x, y) in my_list
-
+    sum_1 = 0
+    sum_2 = 0
+    for (x, y) in my_list:
+        sum_1 += x * y
+        sum_2 += y
+    weight = sum_1 / sum_2
     return weight
