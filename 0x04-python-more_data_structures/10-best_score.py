806bc120388b84ed53fc34769f0ab5dcfe0699f3
Rafiki_Mostafa <rafikimostafa@gmail.com>
2024-01-10 21:44:11 +0100

    Mandatory tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
     Name = list(a_dictionary.keys())[0]
