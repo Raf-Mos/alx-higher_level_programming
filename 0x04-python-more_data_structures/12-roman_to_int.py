7598c9fa0126868b104c29409037609936a5b528
Rafiki_Mostafa <rafikimostafa@gmail.com>
2024-01-10 22:28:48 +0100

    Mandatory tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or len(roman_string) == 0 or
-        not isinstance(roman_string, str):
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     som = 0
     saved_roman = 'I'
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'c': 100, 'D': 500, 'M': 1000}
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'c': 100, 'D': 500, 'M': 1000}
     for n in reversed(range(len(roman_string))):
         if roman[romain_string[n]] >= roman[saved_roman]:
             saved_roman = roman_string[n]
