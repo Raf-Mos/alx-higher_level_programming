f5d4f8348dc43dc114946a92a21dea63b088b0eb
Mostafa.Rafiki <rafikimostafa@gmail.com>
2024-01-23 18:47:57 +0100

    Mandatory tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,60 +11,56 @@ class Square:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
         """
-        if not isinstance(size, int):
+        if not type(size) is int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        elif (not isinstance(position[0], int) or not isinstance(position[1], int)):
+        elif not type(position) is tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (not type(position[0]) is int) or (not type(position[1]) is int):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__size = size
-            sefl.__position = position
-
-    def area(self):
-        """Return the current area of the square"""
-        return (self.__size ** 2)
+            self.__position = position
 
     @property
     def size(self):
-        """get the current size of the square"""
-        return (self.__size)
+        """property to retrieve size"""
+        return self.__size
 
     @size.setter
     def size(self, value):
-        """set a new size for the square"""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
+        """property setter to set size"""
         self.__size = value
 
     @property
     def position(self):
-        """property to retrieve it"""
-        return (self.__position)
+        """property to retrieve position"""
+        return self.__position
 
     @position.setter
     def position(self, value):
-        """property setter to set it"""
+        """property setter to set position"""
         self.__position = value
 
+    def area(self):
+        """Public instance that returns the  square area"""
+        return self.__size * self.__size
+
     def my_print(self):
-        """print the square with '#'"""
+        """Public instance that prints the square with the char #"""
         if self.__size == 0:
             print("")
             return
-
         if self.__position[1] > 0:
             for p in range(0, self.__position[1]):
                 print("")
-
-        for i in range(self.__size):
+        for i in range(0, self.__size):
             if self.__position[0] > 0:
-                for x in range(self.__position[0]):
+                for k in range(0, self.__position[0]):
                     print(" ", end="")
-            for j in range(self.__size):
+            for j in range(0, self.__size):
                 print("#", end="")
             print("")
