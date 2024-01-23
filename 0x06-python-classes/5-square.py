2f193c6722f631ed9ba0a089dc7a85850f14d1a2
Mostafa.Rafiki <rafikimostafa@gmail.com>
2024-01-23 18:25:12 +0100

    Mandatory tasks

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """get the current size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """set a new size for the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """print the square with '#'"""
+        if self.__size == 0:
+            print("")
+            return
+
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
