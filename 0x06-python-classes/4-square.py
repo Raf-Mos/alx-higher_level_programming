eea37e78f91293aed447eece327627f3ca7b0c6e
Mostafa.Rafiki <rafikimostafa@gmail.com>
2024-01-23 18:17:50 +0100

    Mandatory tasks

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,12 +10,22 @@ class Square:
         """Initialize a new Square.
             size (int): The size of the new square.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
+    def area(self):
+        """Return the current area of the square"""
+        return (self.__size ** 2)
+
+    @property
     def size(self):
         """get the current size of the square"""
         return (self.__size)
 
+    @size.setter
     def size(self, value):
         """set a new size for the square"""
         if not isinstance(value, int):
@@ -23,7 +33,3 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """Return the current area of the square"""
-        return (self.__size ** 2)
